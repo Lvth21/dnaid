29224bf78d5ecea5fbd34a72c487f58ec3c8d380
roncl <l.ronci@eureco-eng.it>
2022-11-09 10:38:42 +0100

    footer fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(public router: Router){
+    this.router.events.subscribe((e) => {
+    if(e instanceof NavigationEnd) {
+    window.scroll(0,0);
+    }
+    })
+  }
   title = 'dnaid';
 }
