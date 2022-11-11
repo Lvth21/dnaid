57191da8dbe2f1cfb7aebc234b0d7e2510dc11db
roncl <l.ronci@eureco-eng.it>
2022-11-11 17:39:48 +0100

    show images in programs

--- src/app/classes/ruolo-program.ts
+++ src/app/classes/ruolo-program.ts
@@ -1,7 +1,10 @@
+import { ResourceLoader } from "@angular/compiler";
+
 export class RuoloProgram {
     idRuolo!: number;
     descrizione!: string;
     nome!: string;
+    image!:string;
 
     constructor(args: RuoloProgram) {
         Object.assign(this, args);
--- src/app/classes/tstato.ts
+++ src/app/classes/tstato.ts
@@ -2,6 +2,7 @@ export class Tstato {
     idStato!: number;
     descrizione!: string;
     nome!: string;
+   
 
     constructor(args: Tstato) {
         Object.assign(this, args);
